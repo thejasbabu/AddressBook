a5c66dd350e085ee62ee8275170c052d91071fc3
Thejas <thejasb99@gmail.com>
2018-03-18 13:44:04 +0530

    AddressBook remove and find entries

--- src/main/scala/domain/AddressBook.scala
+++ src/main/scala/domain/AddressBook.scala
@@ -1,7 +1,16 @@
 package domain
 
-case class AddressBook(entries : Set[Entry]) {
+case class AddressBook(entries: Set[Entry]) {
   def add(entry: Entry): AddressBook = {
     AddressBook(this.entries + entry)
+  }
+
+  def remove(entry: Entry): AddressBook = {
+    AddressBook(this.entries - entry)
+  }
+
+  def find(person: Person): Option[Entry] = {
+    val entry = this.entries.filter(_.person.equals(person))
+    entry.headOption
   }
 }
--- src/main/test/domain/AddressBookTest.scala
+++ src/main/test/domain/AddressBookTest.scala
@@ -11,4 +11,37 @@ class AddressBookTest extends FlatSpec with Matchers {
     addressBook.entries.size shouldEqual 1
     addressBook.entries.contains(entry) shouldBe true
   }
+
+  "AddressBook" should "remove an entry from its set of entries" in {
+    val person = Person("mark", "newton", "9987778")
+    val address = Address("Bangalore", "Kar", "577", "#22, 2nd Street")
+    val entry = Entry(person, address)
+    val addressBook = AddressBook(Set(entry))
+    addressBook.entries.size shouldEqual 1
+    val updatedAddressBook = addressBook.remove(entry)
+    updatedAddressBook.entries.size shouldEqual 0
+  }
+
+  "AddressBook" should "not remove an entry from when its not present in its entries" in {
+    val person = Person("mark", "newton", "9987778")
+    val address = Address("Bangalore", "Kar", "577", "#22, 2nd Street")
+    val addressBook = AddressBook(Set(Entry(person, address)))
+    addressBook.entries.size shouldEqual 1
+    val entry = Entry(Person("newton", "mark", "9987778"), address)
+    val updatedAddressBook = addressBook.remove(entry)
+    updatedAddressBook.entries.size shouldEqual 1
+  }
+
+  "AddressBook" should "find entry for a person" in {
+    val mark = Person("mark", "newton", "9987778")
+    val bangalore = Address("Bangalore", "Kar", "577", "#22, 2nd Street")
+    val sam = Person("sam", "newton", "9987778")
+    val mumbai = Address("Mumbai", "MH", "577", "#22, 2nd Street")
+
+    val markEntry = Entry(mark, bangalore)
+    val samEntry = Entry(sam, mumbai)
+    val addressBook = AddressBook(Set(markEntry, samEntry))
+    addressBook.entries.size shouldEqual 2
+    addressBook.find(sam) shouldEqual Some(samEntry)
+  }
 }
